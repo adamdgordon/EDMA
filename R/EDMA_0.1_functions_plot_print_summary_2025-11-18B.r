fd34c4b392171b85b063e725ee8cfc4523cd6fc1
Adam Gordon <adam.d.gordon@gmail.com>
2025-11-18 17:21:03 -0500

    update plotting

--- R/EDMA_0.1_functions_plot_print_summary_2025-11-18B.r
+++ R/EDMA_0.1_functions_plot_print_summary_2025-11-18B.r
@@ -11,13 +11,32 @@
 #' @param tris ...
 #' @param ... Arguments to be passed to other functions.  Not currently used.
 #' @examples
-#' plot_lmks(guenons$rawcoords[,,guenons$species=="patas"][,,30])
-#' plot_lmks(guenons$rawcoords[,,guenons$species=="patas"][,,30],
-#'           segs=guenons$wireframe_segments,
-#' 		     tris=guenons$triangles,
-#' 		     label=TRUE)
+#' plot(talapmean,
+#'      segs=guenons$wireframe_segments,
+#'   	tris=guenons$triangles,
+#'  	label=TRUE, pt.alpha=0.2, pt.size=0.3)
+#' plot.FMmean(patasmean, add=TRUE, offset_z=100,
+#'      segs=guenons$wireframe_segments,
+#'  	tris=guenons$triangles,
+#'  	label=TRUE, pt.alpha=0.2, pt.size=0.3)
 #' @export
-plot_lmks <- function(x, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, segs=NULL, tris=NULL, ...) {
+plot_lmks <- function(x, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, segs=NULL, tris=NULL, 
+                      offset_x=NULL, offset_y=NULL, offset_z=NULL, ...) {
+  if (!is.null(offset_x)) {
+    offset_x <- as.numeric(offset_x)
+    if (is.na(offset_x)) stop("When present, 'offset_x' must be a real number.")
+    x[,1] <- x[,1] + offset_x
+  }
+  if (!is.null(offset_y)) {
+    offset_y <- as.numeric(offset_y)
+    if (is.na(offset_y)) stop("When present, 'offset_y' must be a real number.")
+    x[,2] <- x[,2] + offset_y
+  }
+  if (!is.null(offset_z)) {
+    offset_y <- as.numeric(offset_z)
+    if (is.na(offset_y)) stop("When present, 'offset_z' must be a real number.")
+    x[,3] <- x[,3] + offset_z
+  }
   rgl::plot3d(x,
               aspect="iso",
 			  type="s",
@@ -49,11 +68,14 @@ plot_lmks <- function(x, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE,
 #' @param ... Arguments to be passed to other functions.  Not currently used.
 #' @examples
 #' patasmean<- meanform(guenons$rawcoords[,,guenons$species=="patas"])
+#' talapmean<- meanform(guenons$rawcoords[,,guenons$species=="talapoin"])
 #' plot(patasmean)
-#' plot(patasmean,
+#' plot(talapmean)
+#' plot(talapmean,
 #'      segs=guenons$wireframe_segments,
 #' 		tris=guenons$triangles,
 #' 		label=TRUE, pt.alpha=0.2, pt.size=0.3)
+#' plot(patasmean, add=TRUE)
 #' @export
 plot.FMmean <- function(x, segs=NULL, tris=NULL,
                         pt.cols="blue",
@@ -62,12 +84,15 @@ plot.FMmean <- function(x, segs=NULL, tris=NULL,
 						pt.size.scale=TRUE,
 						pt.alpha=0.5, 
 						label=FALSE, label.cex=0.7,
-						label.adj=1,
+						label.adj=1, 
+                        offset_x=NULL, offset_y=NULL, offset_z=NULL,
 						...) {
   if (pt.col.scale) pt.cols <- colorRampPalette(c("blue", "red"))(256)[round((sqrt(diag(x$SigmaKstar))-min(sqrt(diag(x$SigmaKstar))))/diff(range(sqrt(diag(x$SigmaKstar))))*255,0)+1]
   if (pt.size.scale) pt.size=pt.size*sqrt(diag(x$SigmaKstar))/min(sqrt(diag(x$SigmaKstar)))
   #if (label & pt.size.scale) label.adj=(max(sqrt(diag(x$SigmaKstar))/min(sqrt(diag(x$SigmaKstar)))))^(1/2)
-  plot_lmks(x=x$LMKmean, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size, pt.alpha=pt.alpha, label=label, label.cex=label.cex, ...)
+  plot_lmks(x=x$LMKmean, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size,
+            pt.alpha=pt.alpha, label=label, label.cex=label.cex,
+			offset_x=offset_x, offset_y=offset_y, offset_z=offset_z,...)
 }
 
 #' Plot \code{FM} Object
@@ -85,12 +110,15 @@ plot.FMmean <- function(x, segs=NULL, tris=NULL,
 #' 		tris=guenons$triangles,
 #' 		label=TRUE)
 #' @export
-plot.FM <- function(x, segs=NULL, tris=NULL, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, ...) {
+plot.FM <- function(x, segs=NULL, tris=NULL, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, 
+                    offset_x=NULL, offset_y=NULL, offset_z=NULL, ...) {
   lmks <- cmdscale(x,3)
   colnames(lmks) <- c("X", "Y", "Z")
   rownames(lmks) <- attr(x, "Labels")
   warning("\nForm matrix converted to landmarks using function 'cmdscale' -\n  some axes may be reflected from original configuration.")
-  plot_lmks(lmks, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size, pt.alpha=pt.alpha, label=label, label.cex=label.cex, ...)
+  plot_lmks(lmks, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size,
+            pt.alpha=pt.alpha, label=label, label.cex=label.cex,
+			offset_x=offset_x, offset_y=offset_y, offset_z=offset_z, ...)
 }
 
 #' Plot \code{SM} Object
@@ -108,11 +136,14 @@ plot.FM <- function(x, segs=NULL, tris=NULL, pt.cols="blue", pt.size=0.5, pt.alp
 #' 		tris=guenons$triangles,
 #' 		label=TRUE)
 #' @export
-plot.SM <- function(x, segs=NULL, tris=NULL, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, ...) {
+plot.SM <- function(x, segs=NULL, tris=NULL, pt.cols="blue", pt.size=0.5, pt.alpha=0.5, label=FALSE, label.cex=0.7, 
+                    offset_x=NULL, offset_y=NULL, offset_z=NULL, ...) {
   x <- x*attr(x, "size")
   lmks <- cmdscale(x,3)
   colnames(lmks) <- c("X", "Y", "Z")
   rownames(lmks) <- attr(x, "Labels")
   warning("\nShape matrix rescaled and converted to landmarks using function 'cmdscale' -\n  some axes may be reflected from original configuration.")
-  plot_lmks(lmks, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size, pt.alpha=pt.alpha, label=label, label.cex=label.cex, ...)
+  plot_lmks(lmks, segs=segs, tris=tris, pt.cols=pt.cols, pt.size=pt.size,
+            pt.alpha=pt.alpha, label=label, label.cex=label.cex,
+			offset_x=offset_x, offset_y=offset_y, offset_z=offset_z, ...)
 }
